5c337eb26bfa6fe598d6d32a5d5bc826518df6c4
Niko <ncruzmarsted@stuy.edu>
2017-01-10 11:02:17 -0500

    added intro to Woo and cs1 package for user input

---
+++ Woo.java
@@ -0,0 +1,10 @@
+public class Woo{
+    private static int cash;
+
+
+    public static void main(String[] args){
+	cash = 100;
+	System.out.println("Welcome to the Cruzse Casino! You currently have " + cash + "$ in your account");
+	System.out.println("What game would you like to play? \n  1. Blackjack ");
+    }
+}
