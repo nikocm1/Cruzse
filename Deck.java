7c83a7d3566bd8c4ccd74dd11513b19a1edf2fdc
Jack <jack@Jacks-Air.fios-router.home>
2017-01-12 00:01:38 -0500

    Deck updated, Gambler file started

--- Deck.java
+++ Deck.java
@@ -10,7 +10,8 @@ public class Deck{
     //each card will also get a value from 1 to 13, representing its face value
     //aces will be 1, numbers will be 2-10, jacks 11, queens 12, kings 13
     
-    private Card[] deck;
+    private Card[] deck = new Card[52];
+    private int drawNum = 0;
 
     public Deck(){
 	int count = 0;
@@ -32,5 +33,45 @@ public class Deck{
             deck[rand] = temp;
         }
     }
+
+    //return card at index i of deck
+    public Card get(int i) {
+	return deck[i];
+    }
+
+    //take a card off of the "top" of the deck
+    public Card draw() {
+	Card ret;
+	ret = deck[drawNum];
+	drawNum++;
+	return ret;
+    }
+
+    //reset "top card"
+    //can probably incorporate this
+    //into shuffle
+    public void resetDrawNum() {
+	drawNum = 0;
+    }
+
+    //prints all cards "under" top
+    //card and the top card
+    public String toString() {
+	String ret = "";
+	for (int i = drawNum; i < 52; i++) {
+	    ret += get(i) + "\n";
+	}
+	return ret;
+    }
+
+    /*       
+    public static void main( String[] args ) {
+	Deck d = new Deck();
+	System.out.println(d);
+
+	d.shuffle();
+	System.out.println(d);
+    }
+    */
     
 }
