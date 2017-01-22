03a321b7d69308b2a814e3eefc8641d1c349a45e
Jack <jack@Jacks-Air.fios-router.home>
2017-01-22 18:56:08 -0500

    added card printing help for poker

--- Card.java
+++ Card.java
@@ -80,6 +80,32 @@ public class Card{
 
 	return ret;
     }
+
+    public String cardToString() {
+	String ret = "";
+
+	if (value == 1 || value == 11) {
+	    ret = "A";
+	}
+
+	if (value == 21) {
+	    ret = "J";
+	}
+
+	if (value == 22) {
+	    ret = "Q";
+	}
+
+	if (value == 23) {
+	    ret = "K";
+	}
+
+	else {
+	    ret = value;
+	}
+
+	return ret;
+    }
     
     public String toString(){
 	return (returnValue() + " of " + returnSuit());
